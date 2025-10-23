f07f32ea1867e31bb35e33ff3e8af07937195f3d
Francisco Merchan Cordero <pacomerchanc@gmail.com>
2025-10-23 19:39:02 +0200

    Quirk Update

--- src/data/quirks.js
+++ src/data/quirks.js
@@ -1,17 +1,30 @@
 export const POSITIVE_QUIRKS = [
-  'Beast Hater', 'Beast Slayer', 'Clotter', 'Clutch Hitter', 'Cove Adventurer', 'Deadly', 'Eagle Eye',
-  'Early Riser', 'Eldritch Hater', 'Eldritch Slayer', 'Evasive', 'Gifted', 'Gothic', 'Hard Noggin',
-  'Hippocratic', 'Irrepressible', 'Last Gasp', 'Lurker', 'Luminous', 'Mankind Hater', 'Man Slayer',
-  'Musical', 'Natural Eye', 'Natural Swing', 'Night Owl', 'On Guard', 'Photomania', 'Quick Draw',
-  'Quick Reflexes', 'Robust', 'Slayer of Beasts', 'Slugger', 'Steady', 'Stout', 'Thick Blooded',
-  'Tough', 'Unerring', 'Warrior of Light'
+  'Armor Tinker', 'Back Tracker', 'Balanced', 'Beast Hater', 'Beast Slayer', 'Clotter', 'Clutch Hitter', 
+  'Corvids Eye', 'Corvids Grace', 'Corvids Resilience', 'Cove Adventurer', 'Cove Explorer', 'Cove Scrounger', 'Cove Tactician',
+  'Daredevil', 'Deadly', 'Eagle Eye', 'Early Riser', 'Eldritch Hater', 'Eldritch Slayer', 'Evasive', 
+  'Fairweather Fighter', 'Fast Healer', 'Fated', 'Gifted', 'Gothic', 'Hard Noggin', 'Hard Skinned', 'Healers Gift', 
+  'Hippocratic', 'Hot to Trot', 'Hulk Slayer', 'Irrepressible', 'Last Gasp', 'Luminous', 'Lurker', 'Man Slayer', 
+  'Mankind Hater', 'Mediator', 'Musical', 'Natural', 'Natural Eye', 'Natural Swing', 'Night Owl', 'On Guard', 
+  'Photomania', 'Prismatic Calm', 'Prismatic Coagulation', 'Primatic Eye', 'Prismatic Force', 'Prismatic Isolation', 
+  'Prismatic Precision', 'Prismatic Purity', 'Prismatic Solidity', 'Prismatic Speed', 'Prismatic Stability', 
+  'Quickdraw', 'Quick Reflexes', 'Resillient', 'Robust', 'Ruins Adventurer', 'Ruins Explorer', 'Ruins Scrounger',
+  'Ruins Tactician', 'Scythemaster', 'Second Wind', 'Skilled Gambler', 'Slugger', 'Spiritual', 'Steady', 'Stout', 
+  'Stress Faster', 'Thick Blooded', 'Tough', 'Twilight Dreamer', 'Unerring', 'Unholy Hater', 'Unholy Slayer',
+  'Unyealding', 'Warrens Adventurer', 'Warrens Explorer','Warrens Scrounger', 'Warrens Tactician', 'Warrior of Light', 
+  'Weald Adventurer', 'Weald Explorer', 'Weald Scrounger', 'Weald Tactician', 'Weapon Tinker'
 ];
 
 export const NEGATIVE_QUIRKS = [
-  'Fear of Beasts', 'Weald Phobe', 'Light Sensitive', 'Warrens Phobe', 'Weak Grip on Life', 'Scattering',
-  'Satanophobia', 'Fear of Unholy', 'Soft', 'Fragile', 'Thin Blooded', 'Stress Eater', 'Nervous',
-  'Scientific', 'Torn Rotator', 'Bad Gambler', 'Tuckered Out', 'Ruins Phobe', 'Sickly', 'Ruminator',
-  'Slow Reflexes', 'Slowdraw', 'Weak Grip', 'Phengophobia', 'Off Guard', 'Nocturnal', 'Plutomania',
-  'Guilty Conscience', 'Claustrophobia', 'Fear of Eldritch', 'Fear of Human', 'Fear of Beast', 'Lazy Eye',
-  'Fear of Ruins', 'Fear of Warrens', 'Hagiomania', 'Bloodthirsty', 'Compulsive', 'Kleptomaniac'
+  'Ablutomania', 'Anemic', 'Antsy', 'Ascetic', 'Ashen', 'Automatonophobia', 'Bad Gambler', 'Bad Healer', 'Bloodthirsty',
+  'Calm', 'Claustrophobia', 'Clumsy', 'Compulsive', 'Corvids Appetite', 'Corvids Blindness', 'Corvids Curiosity', 
+  'Cove Phove', 'Curious', 'Dacnomania', 'Dark Temptation', 'Demonomania', 'Deviant Tastes', 'Dipsomania',
+  'Diurnal', 'Dud Hitter', 'Egomania', 'Enlightened', 'Fading', 'Faithless', 'Fear of Beasts', 'Fear of Eldrich',
+  'Fear of Mankind', 'Fear of Unholy', 'Flagellant', 'Fragile', 'Gambler', 'Germophobe', 'God Fearing', 'Guilty Conscience', 
+  'Hagiomania', 'Hieromania', 'Hylomania', 'Imposter Syndrome', 'Infirm', 'Kleptomaniac', 'Known Cheat', 'Lazy Eye', 
+  'Light Sensitive', 'Love Interest', 'Lygophobia', 'Mercurial', 'Misses the Spot', 'Necromania', 'Nervous',
+  'Nervous Bleeder', 'Night Blindness', 'Nocturnal', 'Off Guard', 'Paranormania', 'Perfectionist', 'Phengophobia',
+  'Plutomania', 'Resolution', 'Risktaker', 'Ruins Phobe', 'Ruminator', 'Satanophobia', 'Scattering', 'Scientific', 
+  'Shard Mercenary', 'Shocker', 'Sickly', 'Sitiomania', 'Slow Reflexes', 'Slowdraw',  'Soft', 'Stress Eater', 
+  'Thanatophobia', 'The Yips', 'Thin Blooded', 'Tippler', 'Tone Deaf',  'Torn Rotator', 'Tuckered Out', 'Unquiet Mind', 
+  'Warrens Phobe', 'Weak Grip', 'Weald Phobe', 'Winded', 'Witness', 'Zoophobia'
 ];
